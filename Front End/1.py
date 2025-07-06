428635a3cd27aaf108226b0cd95f20fecc20cb25
Rayudu Roy <Rayudu93>
2025-07-07 00:21:18 +0530

    Added Second Assignment

--- Front End/1.py
+++ Front End/1.py
@@ -1,7 +1,14 @@
 p=[7,1,5,3,6,4]
 ans=0
+minval=p[0]
 for i in range(len(p)-1):
-    for j in range(i+1,len(p)):
-        temp=p[i]-p[j]
-        ans=max(ans,temp)
+    minval=min(minval,p[i])
+    if(p[i]>p[i+1]):
+        temp=p[i]-minval
+        ans+=temp
+        minval=p[i+1]
+    else:
+        if i==len(p)-2:
+            ans+=p[i+1]-minval
+            minval=p[i+1]
 print(ans)
